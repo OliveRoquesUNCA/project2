069ebc2ee6d238c4ea8bec5b8718f2c8ba1a9df4
OliveRoquesUNCA <jroques@unca.edu>
2024-03-26 15:19:51 -0400

    added lucky functionality, test script

--- giphy_cli.py
+++ giphy_cli.py
@@ -1,50 +1,51 @@
-import click
-from giphy_api import GiphyAPI
 import os
 
-API_KEY=os.environ["GIPHY_API_KEY"]
+from giphy_api import GiphyAPI
 
-# TODO:
-# Error handling
-# Implement GiphyCLI class
-
-@click.group()
-def gif():
-    pass
-
-@gif.command()
-@click.option('--count', type=int, default=5, show_default=True, help = "define number of results")
-@click.option('--markdown', is_flag=True, default=False, help = "output in markdown formatting")
-def trending(count, markdown):
-    i = 1
-    trends = GiphyAPI('trending', limit=str(count))
-    results = trends.query(markdown)
-    if(not markdown):
-        for name, result in results.items():
-            click.echo(str(i) + ") " + name + " (" + result + ")")
-            i += 1
-    else:
-        for name, result in results.items():
-            click.echo(str(i) + ") ![" + name + "](" + result + ")")
-            i += 1
-
-@gif.command()
-@click.option('--count', type=int, default=5, show_default=True, help = "define number of results")
-@click.option('--markdown', is_flag=True, default=False, help = "output in markdown formatting")
-@click.argument('query', type=str)
-def search(count, markdown, query):
-    i = 1
-    searches = GiphyAPI('search', limit=str(count), q=query)
-    results = searches.query(markdown)
-    if(not markdown):
-        for name, result in results.items():
-            click.echo(str(i) + ") " + name + " (" + result + ")")
-            i += 1
-    else:
-        for name, result in results.items():
-            click.echo(str(i) + ") ![" + name + "](" + result + ")")
-            i += 1
+API_KEY = os.environ["GIPHY_API_KEY"]
 
 
-if __name__ == "__main__":
-    gif()
+class GiphyCLI:
+
+    def trending(self, count=5, markdown=False, lucky=False):
+        i = 1
+        if lucky:
+            count = 1
+
+        trends = GiphyAPI("trending", limit=str(count))
+        results = trends.query(markdown)
+        output = ""
+        if not markdown:
+            for name, result in results.items():
+                if not lucky:
+                    output += str(i) + ") "
+                output += name + " (" + result + ")\n"
+                i += 1
+        else:
+            for name, result in results.items():
+                if not lucky:
+                    output += str(i) + ") "
+                output += "![" + name + "](" + result + ")\n"
+                i += 1
+        return output
+
+    def search(self, query, count=5, markdown=False, lucky=False):
+        i = 1
+        if lucky:
+            count = 1
+        searches = GiphyAPI("search", limit=str(count), q=query)
+        results = searches.query(markdown)
+        output = ""
+        if not markdown:
+            for name, result in results.items():
+                if not lucky:
+                    output += str(i) + ") "
+                output += name + " (" + result + ")\n"
+                i += 1
+        else:
+            for name, result in results.items():
+                if not lucky:
+                    output += str(i) + ") "
+                output += "![" + name + "](" + result + ")\n"
+                i += 1
+        return output
